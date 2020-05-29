b301d8dfb9c04d7b902bd0b8af4623f8a457b257
superanya <nrth.nrth@yandex.ru>
2020-05-30 01:00:12 +0500

    VERSTKA SOK!!!!!

--- Flashcards.WebAPI/ClientApp/src/components/Home.js
+++ Flashcards.WebAPI/ClientApp/src/components/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import "./Home.css";
 
 export class Home extends Component {
   static displayName = Home.name;
@@ -22,22 +23,27 @@ export class Home extends Component {
     const { decks } = this.state;
 
     return (
-      <div class="card-deck">
-          {
-            decks.map(deck =>
-              <div key={deck.id}>
-                <p>Набор: {deck.name}</p>
-                {
-                    deck.cards.map(card =>
-                      <div class="card text-white bg-dark mb-3">
-                        <p>{card.question} : {card.answer}</p>
-                      </div>
-                    )
-                }
-              </div>)
-          }
-        <Link to="/add">Добавить набор</Link>
+      <div className="card-deck container">
+        {
+          decks.map(deck =>
+            <div key={deck.id} className="col-md-3 col-sm-12 card-container">
+              <div className="card">
+                <div className="card-body">
+                  <h5 className="card-title">{deck.name}</h5>
+                  <p className="card-text">{getCardText(deck)}</p>
+                </div>
+              </div>
+            </div>)
+        }
       </div>
     );
   }
+}
+
+const getCardText = (deck) => {
+  const cards = deck.cards;
+  if (!cards) 
+    return;
+  const firstThree = cards.slice(0, 3).map(c => c.question).join(', ');
+  return cards.length > 3 ? firstThree + "..." : firstThree;
 }
