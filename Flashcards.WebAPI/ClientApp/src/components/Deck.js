41fbdeadc5362b56b21837532716e260ed4b9e24
superanya <nrth.nrth@yandex.ru>
2020-06-02 00:10:50 +0500

    Режим угадывания

--- Flashcards.WebAPI/ClientApp/src/components/Deck.js
+++ Flashcards.WebAPI/ClientApp/src/components/Deck.js
@@ -5,24 +5,36 @@ import { Card } from "./Card.js";
 export class Deck extends Component {
     constructor(props) {
         super(props);
-        this.state = { 
-            cards: [], 
-            indexShownedCard: 0 
+        this.state = {
+            cards: [],
+            indexShownCard: 0
         };
     }
-    
+
     componentDidMount() {
         fetch(`/api/decks/${this.props.match.params.id}`)
             .then(response => response.json())
-            .then(data => this.setState({cards: shuffle(data.cards)}));
+            .then(data => this.setState({ cards: shuffle(data.cards) }));
     }
 
-    render()
-    {
+    render() {
         const { cards } = this.state;
+        if (this.state.cards.length == 0)
+            return (<div className="d-flex justify-content-center">
+                <div className="spinner-border" role="status">
+                    <span className="sr-only">Loading...</span>
+                </div>
+            </div>);
+        if (this.state.indexShownCard >= this.state.cards.length)
+            return (<b>Карточки кончились</b>);
+        const card = cards[this.state.indexShownCard];
         return (
-            <Card card={cards[this.state.indexShownedCard]} /> 
-            );
+            <Card card={card} onNextCard={this.onNextCard} key={card.id} />
+        );
+    }
+
+    onNextCard = () => {
+        this.setState({ indexShownCard: this.state.indexShownCard + 1 });
     }
 
 }
