c2496b94cd68177318ae9ec15ce63e693ccc3cc0
Ratbek Nurlanov <ratbeknurlanov@gmail.com>
2020-06-14 17:27:58 +0500

    Добавить репозиторий карт на Mongo

---
+++ Flashcards.Domain/Repositories/Cards/MongoCardsRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Flashcards.Domain.Entities.Cards;
+using MongoDB.Driver;
+
+namespace Flashcards.Domain.Repositories.Cards
+{
+    public class MongoCardsRepository : ICardsRepository
+    {
+        private readonly IMongoCollection<Card> cardCollection;
+        private const string CollectionName = "cards";
+
+        public MongoCardsRepository(IMongoDatabase db)
+        {
+            cardCollection = db.GetCollection<Card>(CollectionName);
+            cardCollection.Indexes.CreateOne(new CreateIndexModel<Card>(
+                Builders<Card>.IndexKeys.Hashed(card => card.UserId)));
+        }
+
+        public async Task<Guid> AddAsync(Card card)
+        {
+            await cardCollection.InsertOneAsync(card);
+
+            return (await FindAsync(card.Id))?.Id ?? throw new Exception($"Insert card {card.Id} fail");
+        }
+
+        public async Task<Card> GetAsync(Guid id) => await FindAsync(id) ?? throw new Exception($"Not found card {id}");
+
+        public async Task<IEnumerable<Card>> GetUsersCards(Guid userId)
+        {
+            return (await cardCollection.FindAsync(card => card.UserId == userId))
+                .ToEnumerable();
+        }
+
+        private async Task<Card> FindAsync(Guid cardId)
+        {
+            return await (await cardCollection.FindAsync(card => card.Id == cardId))
+                .FirstOrDefaultAsync();
+        }
+    }
+}
